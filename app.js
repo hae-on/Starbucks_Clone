00ee9c6465feb958a359755d71fb52a27ef5d7f5
hae-on <solbi2004@naver.com>
2021-07-17 14:42:45 +0900

    [add]input 전송 & 알람

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
 "use strict";
 
+const navInputForm = document.querySelector(".nav__input__form");
 const navInputDiv = document.querySelector(".nav__input");
 const navBtn = document.querySelector(".small__nav__btn");
 const navInput = document.querySelector(".small__nav__input");
+const inputBtn = document.querySelector(".input__btn");
 
-// function showInputDiv() {
-//   navInputDiv.style.width = "170px";
-//   navInputDiv.style.opacity = 1;
-// }
-
-// function showInput() {
-//   navInput.style.opacity = 1;
-//   navBtn.style.right = "5%";
-// }
-
+// NAV 검색버튼 -> input 변환
 function changeNav() {
   navInputDiv.style.width = "170px";
   navInputDiv.style.opacity = 1;
   navInput.style.opacity = 1;
-  navBtn.style.right = "5%";
-  navBtn.style.border = "none";
+  navBtn.style.display = "none";
+  navBtn.style.right = "-100%";
 }
 
-// navBtn.addEventListener("click", showInputDiv);
-// navBtn.addEventListener("click", showInput);
+// input값 전송하고 알람창
+function inputSubmit(event) {
+  event.preventDefault();
+  const search = navInput.value;
+  if (search !== "") {
+    alert(`${search} 은(는) 검색할 수 없습니다. 다른 검색어를 입력해주세요.`);
+  } else {
+    alert("검색어를 입력하세요.");
+  }
+}
+
 navBtn.addEventListener("click", changeNav);
+navInputForm.addEventListener("submit", inputSubmit);

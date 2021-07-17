1f2d84256d6d04a1584d5c72ac9f1c94dc853c44
hae-on <solbi2004@naver.com>
2021-07-17 21:33:09 +0900

    [add]돋보기 버튼 길게 클릭 시 input창에서 버튼으로 복귀

--- app.js
+++ app.js
@@ -17,6 +17,37 @@ function changeNav() {
   smallNav.style.right = "-35px";
 }
 
+// 길게 클릭 시 버튼으로 복구
+let start = 0;
+function mouseStart() {
+  start = Date.now();
+}
+
+let end = 0;
+function mouseEnd() {
+  end = Date.now();
+  if (end - start > 1000) {
+    navInputDiv.style.opacity = 0;
+    navInput.style.opacity = 0;
+    navBtn.style.display = "block";
+    navBtn.style.right = "0%";
+    smallNav.style.right = "-160px";
+  }
+}
+
+// // 애니메이션
+// let num = 0;
+// function ani() {
+//   setInterval("console.log(num--);", 100);
+//   let timer = setInterval(function () {
+//     smallNav.style.left = "6px";
+//   }, 20);
+//   console.log(timer);
+//   setInterval(function () {
+//     num++;
+//   }, 300);
+// }
+
 // input값 전송하고 알람창
 function inputSubmit(event) {
   event.preventDefault();
@@ -30,3 +61,5 @@ function inputSubmit(event) {
 
 navBtn.addEventListener("click", changeNav);
 navInputForm.addEventListener("submit", inputSubmit);
+inputBtn.addEventListener("mousedown", mouseStart);
+inputBtn.addEventListener("mouseup", mouseEnd);

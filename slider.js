3b0f85793c0623dad5a4a4d6be45374579540a42
hae-on <solbi2004@naver.com>
2021-08-04 21:54:12 +0900

    [add]slide test 파일 등장, 루프, 클론

--- slider.js
+++ slider.js
@@ -1,278 +1,237 @@
-const noticeBtn = document.querySelector(".Notice__promo__btn");
-const slide = document.querySelector(".slide__box");
-const play = document.querySelector(".play");
-const leftArrow = document.querySelector(".arrow__left");
-const rightArrow = document.querySelector(".arrow__right");
-const firstImage = document.querySelector(".banner__1");
-const secondImage = document.querySelector(".banner__2");
-const thirdImage = document.querySelector(".banner__3");
-const slideImage = document.querySelector(".banner__img");
-const banner = document.querySelectorAll(".banner");
-const checkBox = document.querySelectorAll("input[type=checkbox]");
+// const noticeBtn = document.querySelector(".Notice__promo__btn");
+// const slide = document.querySelector(".slide__box");
+// const play = document.querySelector(".play");
+// const leftArrow = document.querySelector(".arrow__left");
+// const rightArrow = document.querySelector(".arrow__right");
+// const firstImage = document.querySelector(".banner__1");
+// const secondImage = document.querySelector(".banner__2");
+// const thirdImage = document.querySelector(".banner__3");
+// const slideImage = document.querySelector(".banner__img");
+// const banner = document.querySelectorAll(".banner");
+// const checkBox = document.querySelectorAll("input[type=checkbox]");
 
-let imageWidth = 819;
-let margin = 20;
+// let imageWidth = 819;
+// let margin = 20;
 
-// slide 등장 & 사라짐
-function showSlide() {
-  slide.classList.toggle("slide__show");
-}
 
-// 이미지 위치 초기값
-function initialPos() {
-  let imagesLength = -(imageWidth + margin) * 4;
-  slideImage.style.transform = " translateX(" + imagesLength + "px)";
-}
+// // 슬라이드 시작 시 첫번째 checkbox checked
+// // changeFirstIcon();
 
-// 슬라이드 시작 시 첫번째 checkbox checked
-changeFirstIcon();
+// let moveCnt = 0;
+// let imageCount = banner.length;
 
-let moveCnt = 0;
-let imageCount = banner.length;
 
-// 이미지 앞 뒤로 clone
-function makeClone() {
-  for (let i = 0; i < imageCount; i++) {
-    let cloneSlide = banner.item(i).cloneNode();
-    slideImage.appendChild(cloneSlide);
-    cloneSlide.classList.add("clone__right");
-  }
-
-  for (let i = 2; i >= 0; i--) {
-    let cloneSlide = banner.item(i).cloneNode();
-    slideImage.prepend(cloneSlide);
-    cloneSlide.classList.add("clone__left");
-  }
-
-  let cloneFirstBanner = document.querySelector(".clone__left");
-  cloneFirstBanner.before(banner.item(2).cloneNode());
-}
-
-setTimeout(function () {
-  slideImage.classList.add("slide__animate");
-}, 100);
-
-// arrow 버튼 효과
-function moveLeft() {
-  slideImage.style.left = slideImage.offsetLeft + 839.5 + "px";
-  moveCnt--;
-  stopSlide();
-  startSlide();
-  if (moveCnt == -imageCount) {
-    setTimeout(function () {
-      slideImage.classList.remove("slide__animate");
-      slideImage.style.left = "-429.5px";
-      moveCnt = 0;
-    }, 500);
-    // 0.1초 후 class 다시 원상복구
-    setTimeout(function () {
-      slideImage.classList.add("slide__animate");
-    }, 600);
-  }
-  // 이미지 변경 시 원 아이콘 변경
-  if (moveCnt === -3) {
-    changeFirstIcon();
-  } else if (moveCnt === -2) {
-    changeSecondIcon();
-  } else if (moveCnt === -1) {
-    changeThirdIcon();
-  }
-
-  // 이동에 따른 투명도 변경
-  if (moveCnt == 1) {
-    banner[1].classList.remove("slide__opacity");
-    banner[2].classList.add("slide__opacity");
-  }
-  if (moveCnt == 0) {
-    banner[0].classList.remove("slide__opacity");
-    banner[1].classList.add("slide__opacity");
-  }
-
-  if (moveCnt == -1) {
-    banner[1].classList.add("slide__opacity");
-    cloneLeft[2].classList.remove("slide__opacity");
-    banner[0].classList.add("slide__opacity");
-    cloneLeft[1].classList.add("slide__opacity");
-  }
-
-  if (moveCnt == -2) {
-    cloneLeft[0].classList.add("slide__opacity");
-    cloneLeft[1].classList.remove("slide__opacity");
-    cloneLeft[2].classList.add("slide__opacity");
-  }
-
-  if (moveCnt == -3) {
-    banner[2].classList.add("slide__opacity");
-    banner[0].classList.remove("slide__opacity");
-    cloneLeft[0].classList.remove("slide__opacity");
-    cloneLeft[1].classList.add("slide__opacity");
-  }
-
-  // console.log("왼쪽 " + moveCnt);
-}
-
-function moveRight() {
-  slideImage.style.left = slideImage.offsetLeft - 839.5 + "px";
-  moveCnt++;
-  stopSlide();
-  startSlide();
-  if (moveCnt == imageCount) {
-    setTimeout(function () {
-      slideImage.classList.remove("slide__animate");
-      slideImage.style.left = "-429.5px";
-      moveCnt = 0;
-    }, 500);
-    // 0.1초 후 class 다시 원상복구
-    setTimeout(function () {
-      slideImage.classList.add("slide__animate");
-    }, 600);
-  }
-  // 이동에 따른 아이콘 변경
-
-  if (moveCnt === 3) {
-    changeFirstIcon();
-  } else if (moveCnt === 1) {
-    changeSecondIcon();
-  } else if (moveCnt === 2) {
-    changeThirdIcon();
-  }
-
-  // 이동에 따른 투명도 변경
-  if (moveCnt == -1) {
-    cloneLeft[2].classList.remove("slide__opacity");
-    cloneLeft[1].classList.add("slide__opacity");
-  }
-
-  if (moveCnt == 0) {
-    banner[0].classList.remove("slide__opacity");
-    cloneLeft[2].classList.add("slide__opacity");
-  }
-
-  if (moveCnt == 1) {
-    banner[0].classList.add("slide__opacity");
-    banner[1].classList.remove("slide__opacity");
-    banner[2].classList.add("slide__opacity");
-  }
-
-  if (moveCnt == 2) {
-    banner[1].classList.add("slide__opacity");
-    banner[2].classList.remove("slide__opacity");
-    banner[0].classList.add("slide__opacity");
-    cloneRight[0].classList.add("slide__opacity");
-  }
-
-  if (moveCnt == 3) {
-    banner[2].classList.add("slide__opacity");
-    banner[0].classList.remove("slide__opacity");
-    banner[1].classList.add("slide__opacity");
-    cloneRight[0].classList.remove("slide__opacity");
-  }
-
-  // console.log("오른쪽" + moveCnt);
-}
-
-// 아이콘 변경
-function changeFirstIcon() {
-  checkBox[0].checked = true;
-  checkBox[1].checked = false;
-  checkBox[2].checked = false;
-}
-
-function changeSecondIcon() {
-  checkBox[1].checked = true;
-  checkBox[0].checked = false;
-  checkBox[2].checked = false;
-}
-
-function changeThirdIcon() {
-  checkBox[2].checked = true;
-  checkBox[0].checked = false;
-  checkBox[1].checked = false;
-}
-
-function startSlide() {
-  moveslide = setInterval(moveRight, 3000);
-}
-
-function stopSlide() {
-  clearInterval(moveslide);
-}
-
-// play -> stop
-let clickCnt = 0;
-function changePlay() {
-  clickCnt++;
-  if (clickCnt % 2 == 1) {
-    play.src = "image/Slide/main_prom_play.png";
-    stopSlide();
-  } else if (clickCnt % 2 == 0) {
-    play.src = "image/Slide/main_prom_stop.png";
-    setTimeout(moveRight, 500);
-    startSlide();
-  }
-}
-
-// const offButton = document.querySelectorAll(".off img");
-
-// // off -> on
-// for (let k = 0; k < offButton.length; k++) {
-//   offButton[k].onclick = function () {
-//     offButton[k].src = "image/Slide/main_prom_on.png";
-//   };
-// }
-
-// 버튼이 0에 있을 때 1번 or 2번 클릭시 moveright
-// 버튼이 1에 있을 때 0번 클릭 시 moveleft, 2번 클릭시 moveright
-// 버튼이 2에 있을 때 0번 or 1번 클릭시 moveleft
-
-// let offFirstCount = 0;
-// let offSecondCount = 0;
-// let offThirdCount = 0;
-
-// offFirst.addEventListener("click", changeFirstOff);
-// offSecond.addEventListener("click", changeSecondOff);
-// offThird.addEventListener("click", changeThirdOff);
-
-// let third = false;
-
-// function changeFirstOff() {
-//   offFirstCount++;
-//   if (offFirstCount % 2 == 1) {
+// // arrow 버튼 효과
+// function moveLeft() {
+//   slideImage.style.left = slideImage.offsetLeft + 839.5 + "px";
+//   moveLeftCnt--;
+//   stopSlide();
+//   startSlide();
+//   
+//   // 이미지 변경 시 원 아이콘 변경
+//   if (moveLeftCnt === -3) {
 //     changeFirstIcon();
-
-//   } else {
-//     offFirst.src = "image/Slide/main_prom_off.png";
-//   }
-// }
-
-// function changeSecondOff() {
-//   offSecondCount++;
-//   if (offSecondCount % 2 == 1) {
+//   } else if (moveLeftCnt === -2) {
 //     changeSecondIcon();
-//   } else {
-//     offSecond.src = "image/Slide/main_prom_off.png";
-//   }
-// }
-
-// function changeThirdOff() {
-//   offThirdCount++;
-//   third = true;
-//   if (offThirdCount % 2 == 1) {
+//   } else if (moveLeftCnt === -1) {
 //     changeThirdIcon();
-//   } else {
-//     offThird.src = "image/Slide/main_prom_off.png";
+//     // } else if (moveLeftCnt === 0) {
+//     //   changeFirstIcon();
+//   }
+
+//   // 이동에 따른 투명도 변경
+//   if (moveLeftCnt == 1) {
+//     banner[1].classList.remove("slide__opacity");
+//     banner[2].classList.add("slide__opacity");
+//   }
+//   if (moveLeftCnt == 0) {
+//     banner[0].classList.remove("slide__opacity");
+//     banner[1].classList.add("slide__opacity");
+//   }
+
+//   if (moveLeftCnt == -1) {
+//     banner[1].classList.add("slide__opacity");
+//     cloneLeft[2].classList.remove("slide__opacity");
+//     banner[0].classList.add("slide__opacity");
+//     cloneLeft[1].classList.add("slide__opacity");
+//   }
+
+//   if (moveLeftCnt == -2) {
+//     cloneLeft[0].classList.add("slide__opacity");
+//     cloneLeft[1].classList.remove("slide__opacity");
+//     cloneLeft[2].classList.add("slide__opacity");
+//   }
+
+//   if (moveLeftCnt == -3) {
+//     banner[2].classList.add("slide__opacity");
+//     banner[0].classList.remove("slide__opacity");
+//     cloneLeft[0].classList.remove("slide__opacity");
+//     cloneLeft[1].classList.add("slide__opacity");
+//   }
+
+//   console.log("왼쪽 " + moveLeftCnt);
+// }
+
+// function moveRight() {
+//   slideImage.style.left = slideImage.offsetLeft - 839.5 + "px";
+//   moveCnt++;
+//   stopSlide();
+//   startSlide();
+//   if (moveCnt == imageCount) {
+//     setTimeout(function () {
+//       slideImage.classList.remove("slide__animate");
+//       slideImage.style.left = "-429.5px";
+//       moveCnt = 0;
+//     }, 500);
+//     // 0.1초 후 class 다시 원상복구
+//     setTimeout(function () {
+//       slideImage.classList.add("slide__animate");
+//     }, 600);
+//   }
+//   // 이동에 따른 아이콘 변경
+
+//   if (moveCnt === 3) {
+//     changeFirstIcon();
+//   } else if (moveCnt === 1) {
+//     changeSecondIcon();
+//   } else if (moveCnt === 2) {
+//     changeThirdIcon();
+//   }
+
+//   // 이동에 따른 투명도 변경
+//   if (moveCnt == -1) {
+//     cloneLeft[2].classList.remove("slide__opacity");
+//     cloneLeft[1].classList.add("slide__opacity");
+//   }
+
+//   if (moveCnt == 0) {
+//     banner[0].classList.remove("slide__opacity");
+//     cloneLeft[2].classList.add("slide__opacity");
+//   }
+
+//   if (moveCnt == 1) {
+//     banner[0].classList.add("slide__opacity");
+//     banner[1].classList.remove("slide__opacity");
+//     banner[2].classList.add("slide__opacity");
+//   }
+
+//   if (moveCnt == 2) {
+//     banner[1].classList.add("slide__opacity");
+//     banner[2].classList.remove("slide__opacity");
+//     banner[0].classList.add("slide__opacity");
+//     cloneRight[0].classList.add("slide__opacity");
+//   }
+
+//   if (moveCnt == 3) {
+//     banner[2].classList.add("slide__opacity");
+//     banner[0].classList.remove("slide__opacity");
+//     banner[1].classList.add("slide__opacity");
+//     cloneRight[0].classList.remove("slide__opacity");
+//   }
+
+//   console.log("오른쪽" + moveCnt);
+// }
+
+// // 아이콘 변경
+// function changeFirstIcon() {
+//   checkBox[0].checked = true;
+//   checkBox[1].checked = false;
+//   checkBox[2].checked = false;
+// }
+
+// function changeSecondIcon() {
+//   checkBox[1].checked = true;
+//   checkBox[0].checked = false;
+//   checkBox[2].checked = false;
+// }
+
+// function changeThirdIcon() {
+//   checkBox[2].checked = true;
+//   checkBox[0].checked = false;
+//   checkBox[1].checked = false;
+// }
+
+// function startSlide() {
+//   moveslide = setInterval(moveRight, 3000);
+// }
+
+// function stopSlide() {
+//   clearInterval(moveslide);
+// }
+
+// // play -> stop
+// let clickCnt = 0;
+// function changePlay() {
+//   clickCnt++;
+//   if (clickCnt % 2 == 1) {
+//     play.src = "image/Slide/main_prom_play.png";
+//     stopSlide();
+//   } else if (clickCnt % 2 == 0) {
+//     play.src = "image/Slide/main_prom_stop.png";
+//     setTimeout(moveRight, 500);
+//     startSlide();
 //   }
 // }
 
-initialPos();
-makeClone();
+// // 0번이 체크 되어 있고 1번을 눌렀을 때, 2번을 눌렀을 때
+// if ((checkBox[0].checked = true)) {
+//   checkBox[1].addEventListener("click", () => {
+//     stopSlide();
+//     slideImage.style.left = slideImage.offsetLeft - 839.5 + "px";
+//     moveRight();
+//   });
+//   // checkBox[2].addEventListener("click", () => {
+//   //   stopSlide();
+//   //   slideImage.style.left = slideImage.offsetLeft - 839.5 * 2 + "px";
+//   //   moveCnt = 2;
+//   //   setTimeout(function () {
+//   //     moveRight();
+//   //   }, 2000);
+//   // });
+// }
 
-// 투명도 조절에 필요한 clone 요소 (위쪽에 적을 수 없음)
-cloneRight = document.querySelectorAll(".clone__right");
-cloneLeft = document.querySelectorAll(".clone__left");
+// // 1번이 체크 되어 있고 0번을 눌렀을 때, 2번을 눌렀을 때
+// if ((checkBox[1].checked = true)) {
+//   // checkBox[0].addEventListener("click", () => {
+//   //   stopSlide();
+//   //   slideImage.style.left = slideImage.offsetLeft + 839.5 + "px";
+//   //   moveCnt = 3;
+//   //   // moveLeft();
+//   //   setTimeout(function () {
+//   //     moveRight();
+//   //   }, 2000);
+//   // });
+//   checkBox[2].addEventListener("click", () => {
+//     stopSlide();
+//     slideImage.style.left = slideImage.offsetLeft - 839.5 + "px";
+//     // moveRight();
+//     // moveLeft();
+//   });
+// }
 
-noticeBtn.addEventListener("click", showSlide);
-play.addEventListener("click", changePlay);
-leftArrow.addEventListener("click", moveLeft);
-rightArrow.addEventListener("click", moveRight);
-noticeBtn.addEventListener("click", startSlide);
+// // 2번이 체크 되어 있고, 0번을 눌렀을 떄, 1번을 눌렀을 때
+// if ((checkBox[2].checked = true)) {
+//   // checkBox[0].addEventListener("click", () => {});
+//   checkBox[1].addEventListener("click", () => {
+//     stopSlide();
+//     slideImage.style.left = slideImage.offsetLeft + 839.5 + "px";
+//   });
+// }
+
+// // checkBox[0].addEventListener("click", () => {
+// //   console.log(checkBox[0].checked);
+// //   slideImage.style.left = slideImage.offsetLeft + 839.5 + "px";
+// // });
+
+// initialPos();
+// makeClone();
+
+// // 투명도 조절에 필요한 clone 요소 (위쪽에 적을 수 없음)
+// cloneRight = document.querySelectorAll(".clone__right");
+// cloneLeft = document.querySelectorAll(".clone__left");
+
+// noticeBtn.addEventListener("click", showSlide);
+// play.addEventListener("click", changePlay);
+// leftArrow.addEventListener("click", moveLeft);
+// rightArrow.addEventListener("click", moveRight);
+// noticeBtn.addEventListener("click", startSlide);
---
+++ slider__test.js
@@ -0,0 +1,91 @@
+const noticeBtn = document.querySelector(".Notice__promo__btn");
+const slide = document.querySelector(".slide__box");
+const play = document.querySelector(".play");
+const leftArrow = document.querySelector(".arrow__left");
+const rightArrow = document.querySelector(".arrow__right");
+const firstImage = document.querySelector(".banner__1");
+const secondImage = document.querySelector(".banner__2");
+const thirdImage = document.querySelector(".banner__3");
+const slideImage = document.querySelector(".banner__img");
+const banner = document.querySelectorAll(".banner");
+const checkBox = document.querySelectorAll("input[type=checkbox]");
+
+let imageWidth = 819;
+let margin = 20;
+let currentIndex = 0;
+
+// slide 등장 & 사라짐
+function showSlide() {
+  slide.classList.toggle("slide__show");
+}
+
+// 이미지 위치 초기값
+function initialPos() {
+  let imagesLength = -(imageWidth + margin) * 4;
+  slideImage.style.transform = " translateX(" + imagesLength + "px)";
+}
+
+// 슬라이드 이동
+function goToSlide(idx) {
+  slideImage.classList.add("animated");
+  slideImage.style.left = -429.5 - 839.5 * idx + "px";
+  currentIndex = idx;
+}
+
+// 이미지 앞 뒤로 clone
+let imageCount = banner.length;
+
+function makeClone() {
+  for (let i = 0; i < imageCount; i++) {
+    let cloneSlide = banner.item(i).cloneNode();
+    slideImage.appendChild(cloneSlide);
+    cloneSlide.classList.add("clone__right");
+  }
+
+  for (let i = 2; i >= 0; i--) {
+    let cloneSlide = banner.item(i).cloneNode();
+    slideImage.prepend(cloneSlide);
+    cloneSlide.classList.add("clone__left");
+  }
+
+  let cloneFirstBanner = document.querySelector(".clone__left");
+  cloneFirstBanner.before(banner.item(2).cloneNode());
+}
+
+setTimeout(function () {
+  slideImage.classList.add("slide__animate");
+}, 100);
+
+// 슬라이드 무한루프
+function loopSlide() {
+  if (currentIndex == imageCount) {
+    loop();
+  } else if (currentIndex == -imageCount) {
+    loop();
+  }
+}
+
+function loop() {
+  setTimeout(function () {
+    slideImage.classList.remove("slide__animate");
+    slideImage.style.left = "-429.5px";
+    goToSlide(0);
+  }, 500);
+  // // 0.1초 후 class 다시 원상복구
+  setTimeout(function () {
+    slideImage.classList.add("slide__animate");
+  }, 600);
+}
+
+makeClone();
+initialPos();
+
+noticeBtn.addEventListener("click", showSlide);
+rightArrow.addEventListener("click", () => {
+  goToSlide(currentIndex + 1);
+  loopSlide();
+});
+leftArrow.addEventListener("click", () => {
+  goToSlide(currentIndex - 1);
+  loopSlide();
+});
